86d87c776da441f9e2656b4825110c8354c4f450
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-05 14:55:49 -0300

    fix: menu layout

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -13,69 +13,71 @@ class Menu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ExpansionTile(
-            title: const Text('POR FASE'),
-            children: [
-              'Postulantado I',
-              'Postulantado II',
-              'Discipulado I',
-              'Discipulado II',
-              'Consagrados'
-            ]
-                .map(
-                  (phase) => ListTile(
-                    title: Text(phase),
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            ExpansionTile(
+              title: const Text('POR FASE'),
+              children: [
+                'Postulantado I',
+                'Postulantado II',
+                'Discipulado I',
+                'Discipulado II',
+                'Consagrados'
+              ]
+                  .map(
+                    (phase) => ListTile(
+                      title: Text(phase),
+                      onTap: () {
+                        if (pop) {
+                          Navigator.pop(context);
+                        }
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => ListPage(phase: phase),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+            ExpansionTile(
+              title: const Text('POR TEMA'),
+              children: categories
+                  .where((cat) => cat != '')
+                  .map(
+                    (cat) => ListTile(
+                      title: Text(cat),
+                      onTap: () {
+                        if (pop) {
+                          Navigator.pop(context);
+                        }
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => ListPage(category: cat),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                  .toList(),
+            ),
+            pop
+                ? ListTile(
+                    leading: const Icon(Icons.home),
+                    title: const Text('INÍCIO'),
                     onTap: () {
                       if (pop) {
                         Navigator.pop(context);
                       }
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => ListPage(phase: phase),
-                        ),
-                      );
+                      Navigator.of(context).pushReplacementNamed('home');
                     },
-                  ),
-                )
-                .toList(),
-          ),
-          ExpansionTile(
-            title: const Text('POR TEMA'),
-            children: categories
-                .where((cat) => cat != '')
-                .map(
-                  (cat) => ListTile(
-                    title: Text(cat),
-                    onTap: () {
-                      if (pop) {
-                        Navigator.pop(context);
-                      }
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => ListPage(category: cat),
-                        ),
-                      );
-                    },
-                  ),
-                )
-                .toList(),
-          ),
-          pop
-              ? ListTile(
-                  leading: const Icon(Icons.home),
-                  title: const Text('INÍCIO'),
-                  onTap: () {
-                    if (pop) {
-                      Navigator.pop(context);
-                    }
-                    Navigator.of(context).pushReplacementNamed('home');
-                  },
-                )
-              : Container(),
-        ],
+                  )
+                : Container(),
+          ],
+        ),
       ),
     );
   }
